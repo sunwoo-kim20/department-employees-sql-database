05743e0b714878c7ed0723d39b2531f3a8402e3b
Sunwoo Kim <s.kim32415@gmail.com>
2021-05-13 10:18:15 -0700

    finished creating tables

--- EmployeeSQL/schemas.sql
+++ EmployeeSQL/schemas.sql
@@ -18,11 +18,35 @@ CREATE TABLE salaries(
 );
 
 CREATE TABLE titles(
-	emp_no int PRIMARY KEY,
-	salary int,
+	emp_no int,
+	title varchar(40) NOT NULL,
 	from_date varchar(10) NOT NULL,
 	to_date varchar(10) NOT NULL,
+	-- creating composite key 
+	PRIMARY KEY (emp_no, title)
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE departments(
+	dept_no varchar(4) PRIMARY KEY,
+	dept_name varchar(30)
+);
+
+CREATE TABLE dept_emp(
+	emp_no int,
+	dept_no varchar(4) NOT NULL,
+	from_date varchar(10) NOT NULL,
+	to_date varchar(10) NOT NULL,
+	-- creating composite key
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no varchar(4) NOT NULL,
+	emp_no int PRIMARY KEY,
+	
 );
 
 -- Importing CSV files
